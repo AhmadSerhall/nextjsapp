3f6afdc72e15ae1bed357e5ec30eb2cd87e87121
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2025-02-04 18:40:20 +0200

    added the necessary imports in graph.js file

---
+++ react-flow-app/src/components/Graph.js
@@ -0,0 +1,4 @@
+"use client";
+import React , {useCallBack ,useState} from "react"; 
+import ReactFlow,{addEdge,Background,Controls,MiniMap,useEdgesState,useNodesState} from "reactflow";
+import "reactflow/dist/style.css";
