18e8c7f4dcb63d87651744d94672f10678712e50
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2025-02-04 18:49:45 +0200

    created initial states for nodes and edges and handled nodes connection

--- react-flow-app/src/components/Graph.js
+++ react-flow-app/src/components/Graph.js
@@ -1,4 +1,16 @@
 "use client";
 import React , {useCallBack ,useState} from "react"; 
-import ReactFlow,{addEdge,Background,Controls,MiniMap,useEdgesState,useNodesState} from "reactflow";
+import ReactFlow,{addEdge,Background,ConnectionLineType,Controls,MiniMap,useEdgesState,useNodesState} from "reactflow";
 import "reactflow/dist/style.css";
+
+const initialNodes =[];
+const initialEdges=[];
+
+const [nodes,setNodes,onNodesChange]=useNodesState(initialNodes);
+const [edges,setEdges,onEdgesChange]=useEdgesState(initialEdges);
+
+const onConnect = useCallBack(
+    (connection)=> setEdges((edges)=>addEdge(ConnectionLineType,edges)),
+    [setEdges]
+);
+
